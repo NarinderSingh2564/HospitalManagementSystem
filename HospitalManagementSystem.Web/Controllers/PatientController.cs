510d604480716db87ae6cbbaf43474bae4d541ba
AMRITPAL SINGH <amritpalbloom@gmail.com>
2025-04-07 15:46:15 +0530

    Changes in the form

---
+++ HospitalManagementSystem.Web/Controllers/PatientController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using HospitalManagementSystem.Models.Models;
+using HospitalManagementSystem.Repository.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalManagementSystem.Web.Controllers
+{
+    public class PatientController : Controller
+    {
+        private readonly IPatientRepository _patientRepository;
+        private readonly IConfiguration _configuration;
+        private readonly IMapper _mapper;
+
+        public PatientController(IPatientRepository patientRepository, IConfiguration configuration, IMapper mapper)
+        {
+            _patientRepository = patientRepository;
+            _configuration = configuration;
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            var patientList = _patientRepository.GetPatientList();
+
+            if (patientList == null)
+            {
+                patientList = new List<PatientModel>(); // Initialize to an empty list if null
+            }
+
+
+            return View(patientList);
+        }
+    }
+}
